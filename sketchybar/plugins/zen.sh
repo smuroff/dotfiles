6529459d72008c1c997533f1e301a04b7750856b
Alex Smurov <a.smurov@berizaryad.ru>
2023-04-21 18:54:32 +0300

    Fix zen mode

--- sketchybar/plugins/zen.sh
+++ sketchybar/plugins/zen.sh
@@ -2,25 +2,29 @@
 
 zen_on() {
   sketchybar --set apple.logo drawing=off \
-             --set '/cpu.*/' drawing=off \
              --set calendar icon.drawing=off \
              --set separator drawing=off \
              --set front_app drawing=off \
              --set volume_icon drawing=off \
+             --set keyboard drawing=off \
+             --set wifi_alias drawing=off \
+             --set battery drawing=off \
              --set spotify.anchor drawing=off \
              --set spotify.play updates=off \
-             --set brew drawing=off \
+             --set brew drawing=off
 }
 
 zen_off() {
   sketchybar --set apple.logo drawing=on \
-             --set '/cpu.*/' drawing=on \
              --set calendar icon.drawing=on \
              --set separator drawing=on \
              --set front_app drawing=on \
              --set volume_icon drawing=on \
+             --set keyboard drawing=on \
+             --set wifi_alias drawing=on \
+             --set battery drawing=on \
              --set spotify.play updates=on \
-             --set brew drawing=on \
+             --set brew drawing=on
 }
 
 if [ "$1" = "on" ]; then
